c25f920ad719eb538340ae32d71dd4983b24ca85
juno-junho <ssmm0205@naver.com>
2022-01-19 23:11:51 +0900

    Times Table

---
+++ baekjoon/for/TimesTable.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class TimesTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        // 1. stream이용
+//        IntStream.range(1,10).forEach(s -> System.out.println(num + " * " + s + " = " + s*num));
+
+        // 2. for문 이융
+        for (int i = 0; i < 10; i++) {
+            System.out.println(num + " * " + i + " = " + num*i);
+        }
+    }
+}
