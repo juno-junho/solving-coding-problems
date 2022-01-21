2c2a3538cb3862698f8a078bb94542a592cf604b
juno-junho <ssmm0205@naver.com>
2022-01-21 23:22:55 +0900

    MinMax using for loop

--- baekjoon/array/MaxMin.java
+++ baekjoon/array/MaxMin.java
@@ -10,10 +10,23 @@ public class MaxMin {
         for (int i = 0; i < array.length; i++) {
             array[i] = sc.nextInt();
         }
-        int max = Arrays.stream(array).max().getAsInt();
-        int min = Arrays.stream(array).min().getAsInt();
+        // 1. stream
+//        int max = Arrays.stream(array).max().getAsInt();
+//        int min = Arrays.stream(array).min().getAsInt();
+//        System.out.println(min);
+//        System.out.println(max);
+        //2. for문
+        int max = array[0];
+        int min = array[0];
+        for (int i = 0; i < array.length ; i++) {
+            if (array[i] > max){
+                max = array[i];
+            }
+            if (array[i] < min){
+                min = array[i];
+            }
+        }
         System.out.println(min);
         System.out.println(max);
-
     }
 }
