af31cfce11ad9e846bb728837d773a8f76a2b0f3
juno-junho <ssmm0205@naver.com>
2022-03-16 10:34:44 +0900

    mar. 16th

--- baekjoon/string/Sangsoo.java
+++ baekjoon/string/Sangsoo.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Sangsoo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] firstNum = sc.next().split("");
-        String[] secondNum = sc.next().split("");
-        
+
+        StringBuffer firstNum = new StringBuffer().append(sc.nextInt()).reverse();
+        StringBuffer secondNum = new StringBuffer().append(sc.nextInt()).reverse();
+
+        if (Integer.parseInt(String.valueOf(firstNum)) > Integer.parseInt(String.valueOf(secondNum))) System.out.println(firstNum);
+        else System.out.println(secondNum);
         // int를 reverse하는 방법.
         // 1. Intstream 이용
         /* Integer::reverse는 비트수를 reverse하는것이기 때문에 사용할 수 없음.*/
