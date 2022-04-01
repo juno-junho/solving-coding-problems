4df611c32662262d55c0347e0169ed50feb64310
juno-junho <ssmm0205@naver.com>
2022-04-02 00:38:54 +0900

    Apr 2nd, 2022

--- baekjoon/Math/Hive.java
+++ baekjoon/Math/Hive.java
@@ -3,14 +3,22 @@ import java.util.Scanner;
 public class Hive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
+        int n = sc.nextInt();
         // 1 7 19 37 61
-        // +6 +12 +18 +24
-
-        long x = 0;
+        // 6 12 18 24 ...
+        int x = 1;
+        int num = 1;
         while (true){
-            x ++;
-
+            if (x ==n) {
+                System.out.println(num);
+                break;
+            }
+            if (x < n && n < x + 6*num){
+                System.out.println(num+1);
+                break;
+            }
+            x += 6*num;
+            num++;
         }
 
     }
