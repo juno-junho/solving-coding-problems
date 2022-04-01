56232a16cb9078090f2e0eaa914c1d6bab6686ec
juno-junho <ssmm0205@naver.com>
2022-04-02 00:00:08 +0900

    April 1st, 2022 correction

--- baekjoon/Math/BreakEvenPoint.java
+++ baekjoon/Math/BreakEvenPoint.java
@@ -3,17 +3,14 @@ import java.util.Scanner;
 public class BreakEvenPoint {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
-        long b = sc.nextLong();
-        long c = sc.nextLong();
-
-        long x = 0;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
         // x대를 판매 한다고 했을때,
-        if (b > c) System.out.println(-1);
+
+        if (b >= c) System.out.println(-1);
         else {
-            while (a + b * x >= c * x) {
-                x++;
-            }
+            int x =a / (c-b) +1;
             System.out.println(x);
         }
     }
---
+++ baekjoon/Math/Hive.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Hive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        // 1 7 19 37 61
+        // +6 +12 +18 +24
+
+        long x = 0;
+        while (true){
+            x ++;
+
+        }
+
+    }
+}
