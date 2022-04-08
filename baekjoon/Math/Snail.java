5e643ccce68825497509c7cb316036ac80c01886
juno-junho <ssmm0205@naver.com>
2022-04-09 00:16:42 +0900

    snail solved

--- baekjoon/Math/Snail.java
+++ baekjoon/Math/Snail.java
@@ -12,13 +12,45 @@ public class Snail {
         // day2 : v + (-a + b) * days
         // day 2 : v -2*a +b <0 면 2출력
         // day 3 : v -3a + 2b <0 이면 3출력
-        // 1000000000 -100*n + 99*(n-1) <0 이면 n 출력
-        // v - a*n + d
+        // 1000000000 -100*n + 99*(n-1) <=0 이면 n 출력 // 1000000000-99 <= n
+        // v - a*n + +b*n -b <=0 -> n 출력
+        // n(b-a) <= b-v
+        // n >= (v-b) / (a-b)
+        //5 1 6 ->  -5 / -4
 
         // 올라가야할 길이 : v  + (-a + b) * days < 0 되는 day 구하면 됨.
         // day> -v /b-a day > x
         // 5 1 6
+        // 7/3  -> 2. xx  2
 
-        System.out.println((-v/(b-a)) + 1   );
+        int result = (v - b) / (a - b);
+        if ( (v - b) % (a - b) == 0) System.out.println(result);
+        else System.out.println(result + 1);
     }
+    // 틀린이유 : Scanner 말고 BufferedReader를 사용하자.
+    /*
+    import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int up = Integer.parseInt(st.nextToken());
+		int down = Integer.parseInt(st.nextToken());
+		int length = Integer.parseInt(st.nextToken());
+
+		int day = (length - down) / (up - down);
+		if ((length - down) % (up - down) != 0)
+			day++;
+
+		System.out.println(day);
+	}
+}
+     */
 }
