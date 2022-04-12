6f65aa5e24d98ae39701e34a583621274a02ce10
juno-junho <ssmm0205@naver.com>
2022-04-12 16:38:52 +0900

    Prime Number solved

--- baekjoon/Math/FindingPrimeNumber.java
+++ baekjoon/Math/FindingPrimeNumber.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.IntStream;
+// 다른방법으로 풀어보기 (시간복잡도 줄이기)
 
 public class FindingPrimeNumber {
     public static boolean isPrime(int num){
@@ -17,7 +18,14 @@ public class FindingPrimeNumber {
         Arrays.setAll(numArr,i-> sc.nextInt());
 //        System.out.println("array : " + Arrays.toString(numArr));
         //IntStrem 사용 // Predicate 다시 공부해보기.
-        long primeCnt = IntStream.of(numArr).filter(FindingPrimeNumber::isPrime).count();
-        System.out.println(primeCnt);
+//        long primeCnt = IntStream.of(numArr).filter(FindingPrimeNumber::isPrime).count();
+//        System.out.println(primeCnt);
+
+        // 2. Stream 사용하지 않고 풀어보기
+        int count = 0;
+        for (int number : numArr) {
+            if (isPrime(number)) count++;
+        }
+        System.out.println(count);
     }
 }
---
+++ baekjoon/Math/PrimeNumber.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class PrimeNumber {
+    public static boolean isPrime(int num){
+        if (num == 1) return false;
+        for (int i = 2; i < num ; i++) {
+            if (num % i ==0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        //IntStream 이용
+//        IntStream primeStream = IntStream.rangeClosed(m, n).filter(PrimeNumber::isPrime);
+//        if (primeStream.count() == 0){
+//            System.out.println(-1);
+//        }else{
+//            IntStream.rangeClosed(m, n).filter(PrimeNumber::isPrime).forEach(System.out::println);
+//        }
+
+        // array사용
+        List<Integer> primeList = new ArrayList<Integer>();
+        while (m <=n){
+            if (PrimeNumber.isPrime(m)){
+                primeList.add(m);
+            }
+            m++;
+        }
+        int sum = 0;
+        for (int num:primeList) {
+            sum += num;
+        }
+        if (sum == 0) System.out.println(-1);
+        else {
+            System.out.println(sum);
+            System.out.println(primeList.get(0));
+        }
+    }
+}
