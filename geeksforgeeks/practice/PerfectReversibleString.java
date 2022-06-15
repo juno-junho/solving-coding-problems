fdd698cf1a73bd07634f23e851e4b08b0d4ee2d0
juno-junho <ssmm0205@naver.com>
2022-06-15 23:33:27 +0900

    jun 15th, 2022, geeksforgeeks

---
+++ geeksforgeeks/practice/PerfectReversibleString.java
@@ -0,0 +1,29 @@
+public class PerfectReversibleString {
+    // This function basically
+// checks if string is
+// palindrome or not
+    static boolean isReversible(String str)
+    {
+        int i = 0, j = str.length() - 1;
+
+        // iterate from
+        // left and right
+        while (i < j)
+        {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    // Driver Code
+    public static void main (String[] args){
+        String str = "aba";
+        if (isReversible(str))
+            System.out.print("YES");
+        else
+            System.out.print( "NO");
+    }
+}
