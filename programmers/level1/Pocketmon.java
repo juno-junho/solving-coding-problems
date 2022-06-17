695fdfaa38aa1b4d96cc22a273aeff06e2e09112
juno-junho <ssmm0205@naver.com>
2022-06-17 22:35:39 +0900

    Jun 17th, programmers

--- programmers/level1/Pocketmon.java
+++ programmers/level1/Pocketmon.java
@@ -1,14 +1,24 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
+import java.util.stream.IntStream;
+//다른 풀이 공부하기
 public class Pocketmon {
 
-    public int solution(int[] nums) {
-        int answer = 0;
-        return answer;  
+    static int solution(int[] nums) {
+        // nums/2 수를 고르는데, 그 중 종류를 많이 뽑을 수 있는 수.
+        // 방법 1. 일단 nums/2 만큼 뽑는데, nums를 set으로 만들어 contains 해서 count++해주는 방법.
+        // 방법 2. distinct()로 중복 없앤다음 그 중 nums.length /2 만큼 뽑기.
+        long count = IntStream.of(nums).distinct().count();
+        if (nums.length / 2 > count) return (int) count;
+        else return (nums.length/2);
+
+//        set.forEach(System.out::println);
+//        Arrays.asList(nums).forEach(System.out::println);
+
     }
     public static void main(String[] args) {
+        int[] nums = {3,3,3,2,2,2};
+        System.out.println(solution(nums));
+
 
     }
 }
