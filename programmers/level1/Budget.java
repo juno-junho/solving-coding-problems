451a091d0e4d3952172d8115c75a53eb0ecb9cf2
juno-junho <ssmm0205@naver.com>
2022-06-22 01:56:27 +0900

    Jun 22th, programmers

--- programmers/level1/Budget.java
+++ programmers/level1/Budget.java
@@ -1,2 +1,22 @@
+import java.util.Arrays;
+
 public class Budget {
+    // 최대 얼마까지 지원 가능하냐...
+    // int 배열 d에서 sort 한 다음에 젤 작은것 부터 돌면서 budget에서 빼주고, 크면 result return하면 됨.
+    static int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int answer = 0;
+        for (int department : d) {
+            if (budget - department < 0) break;
+            budget -= department;
+            answer++;
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[] d = {1,3,2,5,4};
+        int budget = 9;
+        System.out.println(solution(d,budget));
+    }
 }
