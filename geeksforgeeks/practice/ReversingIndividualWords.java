4424ca1194259b189eeff8357729c182d21bb417
juno-junho <ssmm0205@naver.com>
2022-07-08 22:48:52 +0900

    Jul 08th, 2022 geeks for geeks, Reverse words in a given string

--- geeksforgeeks/practice/ReversingIndividualWords.java
+++ geeksforgeeks/practice/ReversingIndividualWords.java
@@ -1,10 +1,13 @@
 public class ReversingIndividualWords {
-    static String reverseWord(String str){
+    // 방법은 정말 많다..
+    static String reverseIndividual(String str){
+        // 방법 1. stack 사용 *** 공부해보기 (지금하기 싫음)
+        // 방법 2. stream사용
+        // 방법 3. 내방법
         StringBuilder sb = new StringBuilder();
         String[] words = str.split(" ");
         for (String word : words) {
-            sb.append(reverseString(word));
-            sb.append(" ");
+            sb.append(reverseString(word)).append(" ");;
         }
         return sb.toString().trim();
     }
@@ -17,8 +20,21 @@ public class ReversingIndividualWords {
         }
         return new String(chars);
     }
+    static String reverseWordsInString(String str){
+        // 방법 1. charArray로 바꾼 다음 for 돌면서 ' '만나면 글자 reverse하기(변수 start, end 설정). 그리고 맨 끝 글자 reverse하기
+        // 방법 2. 내가 한 방법
+        // 방법 3. split으로 쪼개서 단어 한글자씩 돌린다음 String.join()사용해서 붙히기.
+        //  reverse하는 방법이 for로 글자 짝수인지 홀수인지 구분해서 한글자씩 바꿔줌
+        String[] words = str.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length-1; i>=0; i++) {
+            sb.append(words[i]);
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
     public static void main(String[] args) {
-        String s = "Hello World";
-        System.out.println(reverseWord(s));
+        String s = "getting good at coding needs a lot of practice";
+        System.out.println(reverseWordsInString(s));
     }
 }
