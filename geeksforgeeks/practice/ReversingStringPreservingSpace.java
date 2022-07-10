c15b6c8c64d33cf400f6ef5ebcffbee32db7f913
juno-junho <ssmm0205@naver.com>
2022-07-10 17:08:12 +0900

    Jul 10th, 2022, geeks for geeks 'Reverse a string preserving space positions'

--- geeksforgeeks/practice/ReversingStringPreservingSpace.java
+++ geeksforgeeks/practice/ReversingStringPreservingSpace.java
@@ -19,6 +19,36 @@ public class ReversingStringPreservingSpace {
         }
         System.out.println(new String(result));
     }
+    static void reverses2(String str){
+
+        char[] inputArray = str.toCharArray();
+        char[] result = new char[inputArray.length];
+
+        // Mark spaces in result
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] == ' ') {
+                result[i] = ' ';
+            }
+        }
+
+        // Traverse input string from beginning
+        // and put characters in result from end
+        int j = result.length - 1;
+        for (int i = 0; i < inputArray.length; i++) {
+
+            // Ignore spaces in input string
+            if (inputArray[i] != ' ') {
+
+                // ignore spaces in result.
+                if (result[j] == ' ') {
+                    j--;
+                }
+                result[j] = inputArray[i];
+                j--;
+            }
+        }
+        System.out.println(String.valueOf(result));
+    }
     public static void main(String[] args) {
         reverses("internship at geeks for geeks");
     }
