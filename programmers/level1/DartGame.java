4c04fec16c5f9eddf775aa28fb7e98de99dd9f8d
juno-junho <ssmm0205@naver.com>
2022-07-19 12:36:54 +0900

    Jul 19th, 2022, programmers

--- programmers/level1/DartGame.java
+++ programmers/level1/DartGame.java
@@ -1,27 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 // 정규식에 대한 학습 필요.
+// stack으로 풀어보기.. 시간이 많이 걸림.
+// Pattern 과 Matcher 공부하기
 public class DartGame {
-    // 다트 3번 던진다.
-    // 각 기회마다 얻을 수 있는 점수는 0~10점
-    // 점수와 함께 S D T 영역 존재. 각 영역 당첨시 점수에서 1제곱 2제곱 3제곱
-    // * : 스타상 / # : 아차상. 스타상 당첨 시 해당 점수와 바로 전에 얻은 점수를 두 배로 만든다. 아차상 당첨시 해당 점수 마이너스
-    // 단 첫번째 기회에 스타상 나오면 첫번째 스타상 점수만 2배가 됨.
-    // 스타상은 다른 스타상과 중첩가능. 그러면 4배됨. ex) 첫번째 스타상, 두번째도 스타상이면 첫번째는 네배가됨.
-    // 스타상과 아차상 중첩 경우 아차상 점수 -2배
-    // S D T 는 0~10 점까지 점수마다 하나ㅣ씩 존재.
-    // 스타상 / 아차상은 점수마다 둘중 하나만 존재 or 하나도 존재 하지 않음.
-    static int solution(String dartResult) {
-        // 숫자 + 보너스 + 옵션(있을수도 없을수도)
-        int answer = 0;
-        return answer;
+    public int solution(String dartResult) {
+        List<String> list = new ArrayList<>();
+        String replace = dartResult.replace("10","a").replace('S', '1').replace('D', '2').replace('T', '3');
+        StringTokenizer stringTokenizer = new StringTokenizer(replace,"*#",true);
+        while (stringTokenizer.hasMoreTokens()) {
+            String s = stringTokenizer.nextToken();
+            if (s.length() == 4) {
+                list.add(s.substring(0,2));
+                list.add(s.substring(2));
+                continue;
+            }
+            if (s.length() == 6) {
+                list.add(s.substring(0,2));
+                list.add(s.substring(2,4));
+                list.add(s.substring(4));
+                continue;
+            }
+            list.add(s);
+        }
+        int[] scores = new int[3];
+        int index = 0;
+        for (String s : list) {
+            if (s.startsWith("a")) {
+                scores[index] = (int) Math.pow(10, Character.getNumericValue(s.charAt(1)));
+                index++;
+                continue;
+            }
+            if (s.equals("#")) {
+                scores[index - 1] *= -1;
+                continue;
+            }
+            if (s.equals("*")) {
+                scores[index - 1] *= 2;
+                if (index >= 2) scores[index - 2] *= 2;
+                continue;
+            }
+            scores[index] = (int) Math.pow(Character.getNumericValue(s.charAt(0)), Character.getNumericValue(s.charAt(1)));
+            index++;
+        }
+        int sum = 0;
+        for (int score : scores) {
+            sum += score;
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
-        String dartResult = "1S2D*3T";
-        String[] split = dartResult.split("[*|#]");
-        for (String s : split) {
-            System.out.println(s);
-        }
+
     }
 
 }
---
+++ programmers/level1/SecretMap.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+// 비트연산 정리하기
+// string format 정리하기.
+// string replace, replaceAll 사용방법 명확히 알기.(char와 정규식 차이)
+// string 앞에 문자 붙히기 : stringbuilder.insert(0,"문자") 사용
+
+public class SecretMap {
+    static String[] solution(int n, int[] arr1, int[] arr2) {
+      // 1. 일단 arr1, arr2 를 2진수로 변환한다.
+        // 2. 합친다.
+        // 그리고 1인 부분을  #으로 변환해 출력한다.
+        String[] answer = new String[n];
+
+        for (int i = 0; i < arr1.length; i++) {
+            // 비트 연산
+            int num1= arr1[i];
+            int num2= arr2[i];
+            StringBuilder sb = new StringBuilder();
+            String replace = Integer.toBinaryString(num1 | num2).replace('1', '#').replace('0', ' ');
+            for (int j = 0; j < n -replace.length() ; j++) {
+                sb.insert(0," ");
+            }
+            sb.append(replace);
+            answer[i] = sb.toString();
+//            s.replace("1", "#");
+//            StringBuilder sb1 = new StringBuilder();
+//            StringBuilder sb2 = new StringBuilder();
+//            String s1 = Integer.toBinaryString(arr1[i]);
+//            String s2 = Integer.toBinaryString(arr2[i]);
+////            sb.append()
+//            for (int j = 0; j < 5-s1.length() ; j++) {
+//                sb1.append("*");
+//            }
+//            sb1.append(s1);
+//            for (int j = 0; j < 5-s2.length() ; j++) {
+//                sb2.append("*");
+//            }
+//            sb2.append(s2);
+//            char[] chars1 = s1.toCharArray();
+//            char[] chars2 = s2.toCharArray();
+//            for (int j = 0; j < chars1.length; j++) {
+//                if (chars1[i] == '0' &&b chars2[i] == '0')
+//            }
+
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(6, new int[]{46, 33, 33 ,22, 31, 50}, new int[]{27 ,56, 19, 14, 14, 10})));
+//        int num1= 22;
+//        int num2= 14;
+//        System.out.println(Integer.toBinaryString(num1));
+//        System.out.println(Integer.toBinaryString(num2));
+//        System.out.println(Integer.toBinaryString(num1 | num2).replace('1', '#').replace('0',' '));
+    }
+
+
+
+}
