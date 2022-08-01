ab16656f5933cef333c7becef10ca49458fa14f4
juno-junho <ssmm0205@naver.com>
2022-08-01 11:49:29 +0900

    Aug 01st, 2022, programmers level 2 give up

---
+++ programmers/level2/ConvertingParenthesis.java
@@ -0,0 +1,22 @@
+public class ConvertingParenthesis {
+    public String solution(String p) {
+        // p는 이미 균형잡힌 문자열 (와 )의 개수가 동일하다. 길이는 2이상 1000이하인 짝수다.
+
+        // 1. 빈문자열 -> 빈문자열 반환
+        if (p.length() == 0) return p;
+        // 문자열 p를 균형잡힌 괄호 문자열 u와 v로 분리. u는 균형잡힌 괄호 문자열로 더이상 분리 할수 없어야 하며 v는 빈문자열 일 수 있다.
+
+        // u가 올바른 괄호 문자열이면 v에 대해서 1단계 부터 다시 수행. 수행 결과 문자열을 u에 붙인 후 반환
+
+
+        //
+
+
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ programmers/level2/MenuRenewal.java
@@ -0,0 +1,16 @@
+public class MenuRenewal {
+    static String[] solution(String[] orders, int[] course) {
+        // 단품 -> 조합해 코스요리.
+        // 각 손님들이 주문할때 가장 많이 함께 주문한 단품을 코스요리 메뉴로 구성
+        // 코스요리 메뉴 최소 2가지 이상의 단품메뉴로 구성
+        // 최소 2명이상 손님으로 부터 주문된 단품 메뉴 조합에 대해서만 코스요리 메뉴 후보 포함.
+
+        String[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
