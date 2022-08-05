d59c2c325ceca1ad829c84826e6545f69e3457a9
juno-junho <ssmm0205@naver.com>
2022-08-06 00:27:01 +0900

    unsolved

--- programmers/level2/BiggestNum.java
+++ programmers/level2/BiggestNum.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class BiggestNum {
     public String solution(int[] numbers) {
+        // numbers에서 가장 큰 수. (가장
+        Arrays.sort(numbers, new Comparator<Integer>() {
+            @Override
+            public int compare(int o1, int o2) {
+                return (o1-o2);
+            }
+        });
+
+        int[] maxNum = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            while (numbers[i] > 10) {
+                numbers[i] /= 10;
+            }
+            maxNum[i]= numbers[i];
+        }
+        Arrays.sort(maxNum);
+        StringBuilder sb = new StringBuilder();
         String answer = "";
         return answer;
     }
