80ceb883f271f0cabcfc4f8552c542a019682527
juno-junho <ssmm0205@naver.com>
2022-08-20 23:58:26 +0900

    Aug 20th, 2022, baekjoon

--- programmers/level2/BiggestNum.java
+++ programmers/level2/BiggestNum.java
@@ -3,13 +3,13 @@ import java.util.Comparator;
 
 public class BiggestNum {
     public String solution(int[] numbers) {
-        // numbers에서 가장 큰 수. (가장
-        Arrays.sort(numbers, new Comparator<Integer>() {
-            @Override
-            public int compare(int o1, int o2) {
-                return (o1-o2);
-            }
-        });
+//        // numbers에서 가장 큰 수. (가장
+//        Arrays.sort(numbers, new Comparator<Integer>() {
+//            @Override
+//            public int compare(int o1, int o2) {
+//                return (o1-o2);
+//            }
+//        });
 
         int[] maxNum = new int[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
