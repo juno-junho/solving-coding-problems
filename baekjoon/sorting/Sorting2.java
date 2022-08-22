83257ae0e02721a5b41ac59ecd566592e3ce2be7
juno-junho <ssmm0205@naver.com>
2022-08-22 23:32:59 +0900

    Aug 22nd, 2022, sorting

---
+++ baekjoon/sorting/Sorting2.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Sorting2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextLong();
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
