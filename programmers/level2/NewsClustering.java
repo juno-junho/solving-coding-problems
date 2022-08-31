6fadde5a36e4265c84a9dd3ff3c909274e131d8d
juno-junho <ssmm0205@naver.com>
2022-08-31 23:55:30 +0900

    Aug 31st, 2022, programmers level 2

---
+++ programmers/level2/NewsClustering.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+
+public class NewsClustering {
+    public int solution(String str1, String str2) {
+        // 1. 두글자씩 끊어 다중집합을 만든다
+        // 2. 자카드 유사도 (교집합 / 합집합)을 계산한다
+        // 3. 65536을 곱해서 return 한다.
+
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "1asd";
+        System.out.println(s.toUpperCase());
+    }
+}
