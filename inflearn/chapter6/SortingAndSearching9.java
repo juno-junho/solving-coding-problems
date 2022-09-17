1a2a8e4effebd5a46dc90475df6cf258a2524de4
juno-junho <ssmm0205@naver.com>
2022-09-17 23:52:16 +0900

    Sep 17th, 2022, inflearn unsloved git add .git add .!

---
+++ inflearn/chapter6/SortingAndSearching9.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class SortingAndSearching9 {
+    //뻘짓함. 내가 한 풀이는 정렬 한 다음 하는 건데 문제는 정렬 하란말이 없음.
+    int solution(int n, int m, int[] arr) {
+        // idea : 최솟값 (9 : 한장만 담을경우) 최댓값 앨범 모두를 담을 경우 를 이분탐색 돌면서 알맞는지 확인.
+        Arrays.sort(arr);
+        int answer = 0;
+        int lt = arr[n - 1];
+        int rt = 0;
+        for (int i : arr) rt += i;
+        // arr 돌면서 순차적으로 돌면서 mid 값보다 커지기 전까지 담음.
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+//            if (lt == rt) return mid + 1;
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                sum += arr[i];
+                if (sum > mid) {
+                    count++;
+                    sum = arr[i];
+                }
+            }
+            // 담을수 있으면
+            if (count <= m - 1) rt = mid - 1;
+            // 담을 수 없으면
+            else if (count > m - 1) lt = mid + 1;
+        }
+        return rt + 1;
+
+    }
+
+    public static void main(String[] args) {
+        SortingAndSearching9 T = new SortingAndSearching9();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        System.out.println(T.solution(n, m, arr));
+    }
+}
