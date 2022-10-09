d25ee8aeda1f7414626e4806d2c31f27f8c8366a
juno-junho <ssmm0205@naver.com>
2022-10-09 23:29:12 +0900

    Oct 09th, 2022, inflearn greedy algorithms.

---
+++ inflearn/chapter9/Greedy4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Greedy4 {
+
+    public static void main(String[] args) {
+        Greedy4 T = new Greedy4();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] startTime = new int[n];
+        int[] endTime = new int[n];
+        for (int i = 0; i < n; i++) {
+            startTime[i] = sc.nextInt();
+            endTime[i] = sc.nextInt();
+        }
+        T.solution(n, startTime, endTime);
+    }
+
+    private void solution(int n, int[] startTime, int[] endTime) {
+
+    }
+
+}
