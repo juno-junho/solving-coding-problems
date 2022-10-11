31bf66e8c057fa9184ebec392823c0d68fad79f5
juno-junho <ssmm0205@naver.com>
2022-10-12 00:03:05 +0900

    Oct 11th, 2022, inflearn greedy algorithms.

---
+++ inflearn/chapter9/Greedy2_answer.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+import java.util.*;
+class Time1 implements Comparable<Time1>{
+    public int s, e;
+    Time1(int s, int e) {
+        this.s = s;
+        this.e = e;
+    }
+    @Override
+    public int compareTo(Time1 o){
+        if(this.e==o.e) return this.s-o.s;
+        else return this.e-o.e;
+    }
+}
+
+class Greedy2_answer {
+    public int solution(ArrayList<Time1> arr, int n){
+        int cnt=0;
+        Collections.sort(arr);
+        int et=0;
+        for(Time1 ob : arr){
+            if(ob.s>=et){
+                cnt++;
+                et=ob.e;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args){
+        Greedy2_answer T = new Greedy2_answer();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        ArrayList<Time1> arr = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            int x=kb.nextInt();
+            int y=kb.nextInt();
+            arr.add(new Time1());
+        }
+        System.out.println(T.solution(arr, n));
+    }
+}
--- inflearn/chapter9/Greedy4.java
+++ inflearn/chapter9/Greedy4.java
@@ -5,7 +5,6 @@ class Greedy4 {
     public static void main(String[] args) {
         Greedy4 T = new Greedy4();
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         int[] startTime = new int[n];
         int[] endTime = new int[n];
@@ -19,5 +18,4 @@ class Greedy4 {
     private void solution(int n, int[] startTime, int[] endTime) {
 
     }
-
 }
