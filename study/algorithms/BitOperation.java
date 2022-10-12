c33ce052d528e5ee5daec6410734d1d448618389
juno-junho <ssmm0205@naver.com>
2022-10-12 23:58:02 +0900

    Oct 12th, 2022, learning about bitwise operation

--- study/algorithms/BitOperation.java
+++ study/algorithms/BitOperation.java
@@ -1,6 +1,19 @@
 class BitOperation {
     static boolean getBit(int num, int i) {
-        return (num & (i << i)) != 0;
+        return (num & (1 << i)) != 0;
+    }
+
+    static int setBit(int num, int i) {
+        return num | (1 << i);
+    }
+
+    // 해당 자리수 0으로 세팅
+    static int clearBit(int num, int i) {
+        return num & ~(1 << i);
+    }
+
+    static int clearLeftBits(int num, int i) {
+     return num & (1 << i) -1;
     }
 
     public static void main(String[] args) {
