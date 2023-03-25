2018a749ec9b744a307af99b476c4a42eb22d004
juno-junho <ssmm0205@naver.com>
2023-03-25 19:07:14 +0900

    feat(youtube): implementing singlyLinkedList ver.2

---
+++ youtube/SinglyLinkedListVer2.java
@@ -0,0 +1,68 @@
+class SinglyLinkedListVer2 {
+    /**
+     * header를 하나의 빈 노드로 만든다.
+     * -> 첫 노드 조작 가능
+     */
+    Node header;
+
+    static class Node{
+        int data;
+        Node next = null;
+    }
+
+    public SinglyLinkedListVer2() {
+        this.header = new Node();
+    }
+
+    void append(int data) {
+        Node end = new Node();
+        end.data = data;
+        Node head = header;
+        while (head.next != null) {
+            head = head.next;
+        }
+        head.next = end;
+    }
+
+    void remove(int data) {
+        Node head = header;
+        while (head.next != null) {
+            if (head.next.data == data) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+    }
+
+    void print() {
+        // header 다음 부터 출력
+        Node head = header.next;
+        while (head.next != null) {
+            System.out.print(head.data + " -> ");
+            head = head.next;
+        }
+        System.out.println(head.data);
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedListVer2 ll = new SinglyLinkedListVer2();
+
+        ll.append(1);
+        ll.append(2);
+        ll.append(3);
+        ll.append(4);
+        ll.append(5);
+        ll.print();
+
+        // Success
+        ll.remove(1);
+        ll.print();
+
+
+
+
+    }
+
+
+}
